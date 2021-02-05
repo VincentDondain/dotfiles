e83f484ce11da7ad7b1dacc070c2b9baf8464cc3
Vincent Dondain <vincent.dondain@hey.com>
2021-02-05 17:50:08 -0500

    Make zsh default and download zsh-autosuggestions & zsh-syntax-highlighting

--- setup.sh
+++ setup.sh
@@ -13,3 +13,10 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+
+# make zsh the default shell
+chsh -s $(which zsh)
+
+# download 2 important custom oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
