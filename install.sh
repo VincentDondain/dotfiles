c8dc63a17aa62f4ee3834ea08fc173660e683e4c
Vincent Dondain <vincent.dondain@hey.com>
2023-10-09 16:43:55 -0400

    Replace Codespace zshrc by dotfiles one

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ echo "Installing zsh-autosuggestions & zsh-syntax-highlighting"
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+cat .zshrc > $HOME/.zshrc
